2de036bc081e2837cc4511ab005ffa00781bddc3
fobiero <felbiero@gmail.com>
2022-10-17 10:52:07 +0300

    nav debug

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,7 @@
 import style from '../styles/Layout.module.css'
 
 import Nav from './Nav'
+import Footer from './Footer'
 
 const Layout = ({children}) => {
     return(
@@ -11,6 +12,7 @@ const Layout = ({children}) => {
                     {children}
                 </main>
             </div>
+            <Footer />
        </>
     )
 }
--- components/Nav.js
+++ components/Nav.js
@@ -4,12 +4,29 @@ import navStyles from '../styles/Nav.module.css'
 
 const Nav = () => {
     return(
-        <nav className={navStyles.nav}>
-            <ul>
-                <li><Link href='/'>Home</Link></li>
-                <li><Link href='/about'>About</Link></li>
-                <li><Link href='/work'>Work</Link></li>
-            </ul>
+        <nav class="navbar bg-light fixed-top">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Felix Onyango</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                    <div class="offcanvas-header">
+                        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                    </div>
+                    <div class="offcanvas-body">
+                        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                        <li class="nav-item">
+                            <Link href='/about' className={navStyles.nav_link}>About</Link>
+                        </li>
+                        <li class="nav-item">
+                            <Link href='/work' className={navStyles.nav_link}>Project</Link>
+                        </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </nav>
     )
 }
